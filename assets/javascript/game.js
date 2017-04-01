e63664be7414054bb937046c36f7a8d9b58b906c
maria sawaby <maria sawaby>
2017-04-01 15:01:07 -0700

    shows first accurance of letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 
-var options = ["football","basketball","soccer", "tennis", "swimming", "badminton", "cycling", "golf", "hunting", "running", "sailing"];
+var options = ["football","basketball","soccer", "tennis", "swimming", "badminton",
+ "cycling", "golf", "hunting", "running", "sailing"];
 
 // choose a random word
 var wins = 0;
@@ -9,14 +10,44 @@ var i;
 
 var computerGuess = options[Math.floor(Math.random() * options.length)];
 
+arrayComGuess(computerGuess);
+function arrayComGuess(chosenWord){
 
-for(i = 0; i<computerGuess.length; i++){
-	hangman[i] = "-"
-	//document.getElementById("#emptyArray").innerHTML = hangman;
-	document.querySelector("#emptyArray").innerHTML = hangman;
+	for(i = 0; i<chosenWord.length; i++){
+		hangman[i] = "-"
+		//document.getElementById("#emptyArray").innerHTML = hangman;
+		
+
 	
-}
-console.log(hangman);
+	}
+	document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
+	console.log(hangman);
 
+
+	document.onkeyup = function(event){
+		var letter = event.key.toLowerCase();
+		for(i = 0; i<chosenWord.length; i++){
+			if(letter === chosenWord[i]){
+				hangman[chosenWord.indexOf(letter)] = letter;
+				console.log(letter);
+				console.log(hangman);
+				document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
+			}
+			console.log(i);
+		}
+	}
+	
+ }
+
+// function(arr){
+// 	document.onkeyup = function(event){
+// 		var letter = key.event.toLowerCase();
+// 		for(i = 0; i<arr.length; i++){
+// 			if(letter === indexOf(arr[i])){
+
+// 	}
+// }
+// 		}
+		
 // Get elements
   var showLives = document.getElementById("mylives");
