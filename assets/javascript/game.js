8ac68dd90f935287d7d0dc0d7789fba964a6158f
maria sawaby <maria sawaby>
2017-04-02 22:39:31 -0700

    wins work, number of letter typed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,121 @@
 
-var options = ["football","basketball","soccer", "tennis", "swimming", "badminton",
- "cycling", "golf", "hunting", "running", "sailing"];
 
-// choose a random word
 var wins = 0;
-var letterTyped = [];
+//var letterTyped = [];
 var hangman = [];
 var i;
-var numLetterTyped = 0;
+var showLives = 10;
+var options = ["football","basketball","soccer", "tennis", "swimming", "badminton",
+ 	"cycling", "golf", "hunting", "running", "sailing"];
 
-var computerGuess = options[Math.floor(Math.random() * options.length)];
+function game(){
+	// choose a random word
+ 	var computerGuess = options[Math.floor(Math.random() * options.length)];
+ 	document.querySelector("#showLives").innerHTML = showLives;
+ 	document.querySelector("#letterTyped").innerHTML = " ";
+ 	hangmanArray(computerGuess);
+ 	//functions calling
 
-arrayComGuess(computerGuess);
-function arrayComGuess(chosenWord){
+	arrayComGuess(computerGuess);
+	
+}
 
+
+
+function hangmanArray(chosenWord){
+	hangman = [];
+	// initializing hangman array with "_"
 	for(i = 0; i<chosenWord.length; i++){
 		hangman[i] = "-"
-		//document.getElementById("#emptyArray").innerHTML = hangman;
-		
-
-	
 	}
+	// printing the initial values or hangman
 	document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
-	console.log(hangman);
+}
 
 
+
+
+function arrayComGuess(chosenWord){
+
+	document.querySelector("#wins").innerHTML = wins;
+
+	// when user press a key, on key up this statement triggers the event
 	document.onkeyup = function(event){
+		// any letter changed to lowercase
 		var letter = event.key.toLowerCase();
+		// if(typeof(letter) !== String){
+			// document.getElementById("alert").innerHTML = "Please Enter a letter";
+		// }
 
+		//letter typed so far
+		document.querySelector("#letterTyped").innerHTML += letter.toUpperCase();
+
+		// iterate over the random word to find if the entered key is in there
 		for(i = 0; i<chosenWord.length; i++){
 			if(letter === chosenWord[i]){
+				// second for loop looks for duplicate letters and insert them
 				for(var j =0; j<chosenWord.length; j++){
 					if(chosenWord[j] == letter){
 						hangman[j] = letter;
 					}
 				}
-				// hangman[chosenWord.indexOf(letter)] = letter;
-				// console.log(letter);
-				// console.log(hangman);
+				//prints each character which are entered correctly
 				document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
-				// showLives -=1;
-				// document.querySelector("#showLives").innerHTML = showLives;
 			}
-			// if(chosenWord == hangman){
-			// 	wins +=1;
-				
-			// }
-			console.log("wins"+wins);
+			// showLives(letter, chosenWord);
+			
 		}
+		// if(!(letter == chosenWord[i])){
+		// 	showLives -=1;
+		// 	console.log(showLives);
+		// 	document.querySelector("#showLives").innerHTML = showLives;
+				
+		// } 
+		var count = 0;
+		for(i = 0; i<chosenWord.length; i++){
 
-	};
+			if(chosenWord[i] === hangman[i]){
+				count++;
+
+			}
+
+			if(count == chosenWord.length){
+				wins++;
+				document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
+				
+				//wait 3 second and reset
+				setTimeout(game, 1000);
+
+
+			}
+
+		}
 	
- }
-// ["Nano","Volvo","BMW","Nano","VW","Nano"].reduce(function(a, e, i) {
-//     if (e === 'Nano')
-//         a.push(i);
-//     return a;
-// }, []); 
-// function(arr){
-// 	document.onkeyup = function(event){
-// 		var letter = key.event.toLowerCase();
-// 		for(i = 0; i<arr.length; i++){
-// 			if(letter === indexOf(arr[i])){
+	document.querySelector("#wins").innerHTML = wins;
+	};
+}//function end
 
-// 	}
-// }
-// 		}
+//this function calculates how many times does the user entered the letter wrongly
+function lives(letter, cGuess){
+	for(i = 0; i<15; i++){
+		for(var j = 0; j<cGuess.length; j++){
+			if(letter !== cGuess[i]){
+				showLives -=1;
+				break;
+			}
+		}document.querySelector("#showLives").innerHTML = showLives;
 		
-// Get elements
-  var showLives = document.getElementById("mylives");
+	}
+	if(showLives == 0){
+		document.getElementById("#alert").innerHTML = alert("You have no more chances to guess! <br> Play Again.");
+		game();
+	}
+
+}
+game();
+// document.getElementById('reset').onclick = function() {
+// 	hangmanArray(computerGuess); 
+// 	arrayComGuess(computerGuess);	
+// };	
+// lives(computerGuess);
+  //var showLives = document.getElementById("mylives");
