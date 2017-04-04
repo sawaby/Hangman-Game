d03c06d3e8f59c14e5af1148ec33eede2c7bc3d7
maria sawaby <maria sawaby>
2017-04-04 13:39:08 -0700

    working version, adding bonnus part in next version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,22 @@
 
 
-var wins = 0;
-//var letterTyped = [];
-var hangman = [];
+var wins = 0; 
+var hangman = []; //empty array to be filled after user guesses
 var i;
-var showLives = 10;
+var showLives = 15;
 var options = ["football","basketball","soccer", "tennis", "swimming", "badminton",
  	"cycling", "golf", "hunting", "running", "sailing"];
+var chosenWord;
 
-function game(){
+function resetGame(){
 	// choose a random word
- 	var computerGuess = options[Math.floor(Math.random() * options.length)];
+ 	chosenWord = options[Math.floor(Math.random() * options.length)];
+ 	showLives = 15;
  	document.querySelector("#showLives").innerHTML = showLives;
  	document.querySelector("#letterTyped").innerHTML = " ";
- 	hangmanArray(computerGuess);
  	//functions calling
-
-	arrayComGuess(computerGuess);
-	
+ 	hangmanArray(chosenWord);
+ 	document.querySelector("#wins").innerHTML = wins;
 }
 
 
@@ -26,96 +25,59 @@ function hangmanArray(chosenWord){
 	hangman = [];
 	// initializing hangman array with "_"
 	for(i = 0; i<chosenWord.length; i++){
-		hangman[i] = "-"
+		hangman[i] = "_"
 	}
 	// printing the initial values or hangman
 	document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
 }
 
 
+	
 
-
-function arrayComGuess(chosenWord){
-
-	document.querySelector("#wins").innerHTML = wins;
-
-	// when user press a key, on key up this statement triggers the event
-	document.onkeyup = function(event){
-		// any letter changed to lowercase
-		var letter = event.key.toLowerCase();
-		// if(typeof(letter) !== String){
-			// document.getElementById("alert").innerHTML = "Please Enter a letter";
-		// }
-
-		//letter typed so far
+// when user press a key, on key up this statement triggers the event
+document.onkeyup = function(event){
+	// any letter changed to lowercase
+	var letter = event.key.toLowerCase();
+	//check if keypressed is actually a letter pressed
+	if((event.keyCode >= 65 && event.keyCode <= 90)){
+		//display letter typed so far in uppercase
 		document.querySelector("#letterTyped").innerHTML += letter.toUpperCase();
 
 		// iterate over the random word to find if the entered key is in there
 		for(i = 0; i<chosenWord.length; i++){
 			if(letter === chosenWord[i]){
-				// second for loop looks for duplicate letters and insert them
-				for(var j =0; j<chosenWord.length; j++){
-					if(chosenWord[j] == letter){
-						hangman[j] = letter;
-					}
-				}
-				//prints each character which are entered correctly
-				document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
-			}
-			// showLives(letter, chosenWord);
+				if(chosenWord[i] == letter){
+					hangman[i] = letter;
+				}	
+			}		
+		}
+		//display the hangman array	
+		document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
+
+		/**checks the number of chances the user has, and decrease the number of chances
+		if the user enteres a wrong guess**/
+		if(chosenWord.indexOf(letter) == -1){
+			showLives -=1;
+			document.querySelector("#showLives").innerHTML = showLives;
+		}
+		//if number of chances equals 0 reset the game
+		if(showLives < 1){
+			alert("You have no more chances to guess!  Play Again.");
+			resetGame();
+		}
 			
-		}
-		// if(!(letter == chosenWord[i])){
-		// 	showLives -=1;
-		// 	console.log(showLives);
-		// 	document.querySelector("#showLives").innerHTML = showLives;
-				
-		// } 
-		var count = 0;
-		for(i = 0; i<chosenWord.length; i++){
-
-			if(chosenWord[i] === hangman[i]){
-				count++;
-
-			}
-
-			if(count == chosenWord.length){
-				wins++;
-				document.querySelector("#emptyArray").innerHTML = hangman.join(" ");
-				
-				//wait 3 second and reset
-				setTimeout(game, 1000);
-
-
-			}
-
-		}
-	
-	document.querySelector("#wins").innerHTML = wins;
-	};
-}//function end
-
-//this function calculates how many times does the user entered the letter wrongly
-function lives(letter, cGuess){
-	for(i = 0; i<15; i++){
-		for(var j = 0; j<cGuess.length; j++){
-			if(letter !== cGuess[i]){
-				showLives -=1;
-				break;
-			}
-		}document.querySelector("#showLives").innerHTML = showLives;
-		
+		//user wins if enteres the word correctly, show number of wins
+		if (hangman.join("") == chosenWord) {
+			wins++;
+			document.querySelector("#wins").innerHTML = wins;
+			//if the user wins, wait 1 sec, then reset the game
+			setTimeout(resetGame, 1000);
+		}	
 	}
-	if(showLives == 0){
-		document.getElementById("#alert").innerHTML = alert("You have no more chances to guess! <br> Play Again.");
-		game();
+	else {
+		alert("Please Enter a letter");
 	}
+};
 
-}
-game();
-// document.getElementById('reset').onclick = function() {
-// 	hangmanArray(computerGuess); 
-// 	arrayComGuess(computerGuess);	
-// };	
-// lives(computerGuess);
-  //var showLives = document.getElementById("mylives");
+//calling the resetGame function will restart the game
+resetGame();
